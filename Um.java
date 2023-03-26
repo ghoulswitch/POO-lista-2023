722955fdc182360035073147af10362af0aedae3
jm! <127960462+ghoulswitch@users.noreply.github.com>
2023-03-26 10:23:49 -0300

    Create Um.java

---
+++ Um.java
@@ -0,0 +1,29 @@
+package poolistaum;
+import java.util.Scanner;
+
+public class Um {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		double a, b, c;
+		double soma, media;
+		
+		System.out.println("Digite a primeira nota:");
+		a = sc.nextDouble();
+		System.out.println("Digite a segunda nota:");
+		b = sc.nextDouble();
+		System.out.println("Digite a terceira nota:");
+		c = sc.nextDouble();
+		
+		soma = a + b + c;
+		media = soma / 2;
+		
+		System.out.printf("O valor da média é: %.1f\n",media);
+		
+		sc.close();
+		
+	}
+	
+}
