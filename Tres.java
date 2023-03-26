53c835cdc2233a4643b755e0ab91b2728e07f284
jm! <127960462+ghoulswitch@users.noreply.github.com>
2023-03-26 10:25:04 -0300

    Create Tres.java

---
+++ Tres.java
@@ -0,0 +1,18 @@
+package poolistaum;
+
+public class Tres {
+	
+	public static void main (String[] args) {
+		
+		double a = 1;
+		double b = 2;
+		double c = 3;
+		double mediaponderada;
+		
+		mediaponderada = (a * 1 + b * 2 + c * 2) / 5;
+		
+		System.out.printf("A média ponderada é: %.1f", mediaponderada);
+				
+	}
+
+}
