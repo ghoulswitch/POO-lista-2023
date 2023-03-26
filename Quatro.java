ca491fe3c08a61ddb6f7461a7910582839e421ce
jm! <127960462+ghoulswitch@users.noreply.github.com>
2023-03-26 10:25:42 -0300

    Create Quatro.java

---
+++ Quatro.java
@@ -0,0 +1,18 @@
+package poolistaum;
+
+public class Quatro {
+	
+	public static void main(String[] args) {
+		
+		int val1 = 1;
+		int val2 = 2;
+		int val3 = 3;
+		int val4 = 4;
+		double mediaponderada;
+		
+		mediaponderada = (val1 * 1 + val2 * 2 + val3 * 2 + val4 * 2) / 5;
+		
+		System.out.printf("A média ponderada é: %.1f", mediaponderada);
+	}
+
+}
